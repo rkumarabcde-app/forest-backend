3fb75b46cd6dab4b552a95ae3a96a3c645181af7
rkumarabcde-app <rkumarabcde@gmail.com>
2026-03-24 15:42:27 +0530

    Restores to older version of removed geopandas

--- main.py
+++ main.py
@@ -225,8 +225,6 @@ async def forest_loss(
         # ---------------------------
 
         forest_loss = ndvi_change.lt(-0.2).selfMask()
-        connected = forest_loss.connectedPixelCount(500, True)
-        forest_loss = forest_loss.updateMask(connected.gte(10))
 
         # Connected components
         clusters = forest_loss.connectedComponents(
