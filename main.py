d17c35e4d91777f1679da6c619815e24aee11e1e
rkumarabcde-app <rkumarabcde@gmail.com>
2026-03-24 15:20:21 +0530

    Restored older version ("Filtered values")

--- main.py
+++ main.py
@@ -189,20 +189,6 @@ async def forest_loss(
             buffer.write(await kml.read())
 
         aoi = kml_to_ee_geometry(kml_path)
-        aoi_area_m2 = aoi.area(maxError=1)
-        aoi_area_ha = aoi_area_m2.divide(10000)
-        area_ha = aoi_area_ha.getInfo()
-
-        if area_ha < 2000:
-            min_pixels = 10      # small block / village
-        elif area_ha < 10000:
-            min_pixels = 30      # sub-district
-        elif area_ha < 25000:
-            min_pixels = 50
-        elif area_ha < 50000:
-            min_pixels = 100     # district
-        else:
-            min_pixels = 1000     # very large district/state
 
         # ---------------------------
         # Date ranges (Oct–Nov window)
@@ -240,7 +226,7 @@ async def forest_loss(
 
         forest_loss = ndvi_change.lt(-0.2).selfMask()
         connected = forest_loss.connectedPixelCount(500, True)
-        forest_loss = forest_loss.updateMask(connected.gte(min_pixels))
+        forest_loss = forest_loss.updateMask(connected.gte(10))
 
         # Connected components
         clusters = forest_loss.connectedComponents(
